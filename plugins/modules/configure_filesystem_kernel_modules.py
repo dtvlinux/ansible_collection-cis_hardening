73366af330fb13c2053b258c3219d346ce4a8255
michael lucraft <>
2025-12-31 14:51:39 +0000

    major rewrite to be layed out better

--- plugins/modules/configure_filesystem_kernel_modules.py
+++ plugins/modules/configure_filesystem_kernel_modules.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python
 
-# Copyright: (c) 2025, Michael Lucraft @dtvlinux
+# Copyright: (c) 2026, Michael Lucraft @dtvlinux
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
-from __future__ import absolute_import, division, print_function
-__metaclass__ = type
+from __future__ import annotations
 
 DOCUMENTATION = r'''
 ---
 module: configure_filesystem_kernel_modules
-
-short_description: Configure filesystem kernel modules
-
+short_description: Disable filesystem kernel modules
+author:
+  - Michael Lucraft (@dtvlinux)
 version_added: "1.0.0"
-
-description: This module creates or updates a configuration file in /etc/modprobe.d/ to disable specified kernel modules by blacklisting them and setting install to /bin/false. It also unloads the modules if they are currently loaded. Special handling for 'squashfs' by skipping configuration and unloading if it is in use (e.g., via snap packages) or built into the kernel. The module is idempotent and supports check mode. If 'squashfs' is the only module and it is skipped, the config file is not created if it does not exist.
-
+description:
+  - "This module manages a modprobe configuration file to disable specified kernel modules (typically filesystem-related) by adding 'install' and 'blacklist' directives."
+  - "It ensures the configuration file exists with correct ownership (root:root), mode (0644), and a managed header."
+  - "If modules are currently loaded, it unloads them."
+  - "Special handling for 'squashfs': skips configuration if in use (e.g., /snap/ mounted) or built into the kernel."
+  - "Supports check mode to validate without changes."
 options:
   config_file:
     description:
@@ -29,247 +32,289 @@ options:
     required: true
     type: list
     elements: str
-
-author:
-  - Michael Lucraft (@dtvlinux)
 '''
 
 EXAMPLES = r'''
-
-# Specify single module for disabling
-- name: Disable cramfs
+- name: Specify single module for disabling
   dtvlinux.cis_hardening.configure_filesystem_kernel_modules:
     modules: cramfs
 
-# Specifiy multiple modules to be disabled
-- name: Disable cramfs & freevxfs
+- name: Specifiy multiple modules to be disabled
   dtvlinux.cis_hardening.configure_filesystem_kernel_modules:
     modules:
       - cramfs
       - freevxfs
 
-# Specify a custom configuration file
-- name: Disable cramfs
+- name: Specify a custom configuration file
   dtvlinux.cis_hardening.configure_filesystem_kernel_modules:
     config_file: /etc/modprobe.d/cramfs.conf
     modules: cramfs
 '''
 
 RETURN = r'''
+---
 changed:
-  description: Whether the module made any changes to the system.
+  description: Whether any changes were made.
   type: bool
   returned: always
-
-message:
-  description: A status message describing the actions taken.
+msg:
+  description: Summary message of actions taken or skipped.
   type: str
   returned: always
-
-unloaded_modules:
-  description: List of modules that were unloaded during the run.
-  type: list
-  elements: str
+validation_results:
+  description: Dictionary of validation states for the configuration.
+  type: dict
   returned: always
-
-skipped_modules:
-  description: List of modules that were skipped (e.g., squashfs if in use).
-  type: list
-  elements: str
-  returned: when verbosity >=1
-
-debug_message:
-  description: Additional debug information about why modules were skipped.
-  type: str
-  returned: when verbosity >=1
+  contains:
+    exists:
+      description: Whether the config file exists.
+      type: bool
+    mode:
+      description: Whether the file mode is 0644.
+      type: bool
+    user:
+      description: Whether the file owner is root (UID 0).
+      type: bool
+    group:
+      description: Whether the file group is root (GID 0).
+      type: bool
+    header:
+      description: Whether the file starts with the managed header.
+      type: bool
+    contents:
+      description: Dictionary where keys are module names and values indicate if the module is correctly configured (install and blacklist lines present, no other mentions).
+      type: dict
+    unloaded:
+      description: Dictionary where keys are module names and values indicate if the module is unloaded.
+      type: dict
 '''
 
 import os
-import re
-import subprocess
 
 from ansible.module_utils.basic import AnsibleModule
 
-def run_module():
-    module_args = dict(
-        config_file=dict(
-            type='str',
-            required=False,
-            default='/etc/modprobe.d/cis_hardening.conf'
-        ),
-        modules=dict(
-            type='list',
-            elements='str',
-            required=True
-        ),
-    )
+class ConfigureFilesystemKernelModules:
+    FILE_HEADER = '# Managed by Ansible collection dtvlinux.cis_hardening'
+    FILE_MODE   = 0o644
+    FILE_UID    = 0
+    FILE_GID    = 0
 
-    result = dict(
-        changed=False,
-        message='',
-        unloaded_modules=[],
-        skipped_modules=[],
-        debug_message='',
-    )
+    def __init__(self, module):
+        self.module      = module
+        self.config_file = module.params['config_file']
+        self.modules     = module.params['modules']
+    
+    def filter_modules(self):
+        if 'squashfs' not in self.modules:
+            return None
 
-    module = AnsibleModule(
-        argument_spec=module_args,
-        supports_check_mode=True
-    )
+        squashfs_in_use  = '/snap/' in open('/proc/mounts').read()
 
-    config_file = module.params['config_file']
-    modules_list = module.params['modules']
-    changed = False
-    content_changed = False
-    comment = '# Managed by Ansible collection dtvlinux.cis_hardening'
+        builtin_path = f'/lib/modules/{os.uname()[2]}/modules.builtin'
+        squashfs_builtin = 'squashfs' in open(builtin_path).read() if os.path.exists(builtin_path) else False
 
-    squashfs_in_use = False
-    squashfs_builtin = False
+        if squashfs_in_use or squashfs_builtin:
+            if len(self.modules) == 1:
+                self.module.exit_json(changed=False, msg="skipped squashfs as in use")
+            
+            self.modules = [m for m in self.modules if m != 'squashfs']
+            return "skipped squashfs as in use"
+        
+        return None
 
-    try:
-        snap_output = subprocess.check_output(['lsblk', '-o', 'MOUNTPOINT'], text=True)
-        snap_count = sum(1 for line in snap_output.splitlines() if '/snap' in line.strip())
-        if snap_count > 0:
-            squashfs_in_use = True
-    except Exception:
-        pass
+    def check_config_file_exists(self):
+        return os.path.exists(self.config_file)
+    
+    def check_config_file_mode(self):
+        return (os.stat(self.config_file).st_mode & 0o777) == self.FILE_MODE
 
-    try:
-        kernel = os.uname()[2]
-        builtin_path = f'/lib/modules/{kernel}/modules.builtin'
-        if os.path.exists(builtin_path):
-            with open(builtin_path, 'r') as f:
-                for line in f:
-                    if 'squashfs' in line:
-                        squashfs_builtin = True
-                        break
-    except Exception as e:
-        module.fail_json(msg=f"Failed to check if squashfs is built-in: {str(e)}", **result)
+    def update_config_file_mode(self):
+        os.chmod(self.config_file, self.FILE_MODE)
 
-    skip_squashfs = squashfs_in_use or squashfs_builtin
+    def check_config_file_user(self):
+        return os.stat(self.config_file).st_uid == self.FILE_UID
 
-    if skip_squashfs and 'squashfs' in modules_list:
-        result['skipped_modules'].append('squashfs')
-        result['debug_message'] = 'squashfs is in use (snap packages or built-in kernel module) - skipped configuration and unloading.'
-        modules_list = [m for m in modules_list if m != 'squashfs']
+    def check_config_file_group(self):
+        return os.stat(self.config_file).st_gid == self.FILE_GID
+    
+    def update_config_file_ownership(self):
+        os.chown(self.config_file, self.FILE_UID, self.FILE_GID)
+    
+    def check_config_file_header(self):
+        with open(self.config_file, 'r') as f:
+            return f.readline().strip() == self.FILE_HEADER
 
-    try:
-        with open(config_file, 'r') as f:
+    def update_config_file_header(self):
+        with open(self.config_file, 'r') as f:
             lines = f.readlines()
-        file_exists = True
-    except FileNotFoundError:
-        lines = []
-        file_exists = False
 
-    owner_needs_change = False
-    mode_needs_change = False
-    if file_exists:
-        stat_info = os.stat(config_file)
-        current_uid = stat_info.st_uid
-        current_gid = stat_info.st_gid
-        current_mode = stat_info.st_mode & 0o777
+        if not lines:
+            lines = [f"{self.FILE_HEADER}\n"]
+        elif lines[0].strip().startswith(('install', 'blacklist')):
+            lines = [f"{self.FILE_HEADER}\n"] + lines
+        else:
+            lines = [f"{self.FILE_HEADER}\n"] + lines[1:]
 
-        owner_needs_change = (current_uid != 0 or current_gid != 0)
-        mode_needs_change = (current_mode != 0o644)
+        with open(self.config_file, 'w') as f:
+            f.writelines(lines)
+    
+    def check_config_file_contents(self):
+        with open(self.config_file, 'r') as f:
+            file_lines = [line.strip() for line in f.readlines()]
 
-    if owner_needs_change or mode_needs_change:
-        changed = True
+        module_status = {}
+        for module_name in self.modules:
+            install_line   = f"install {module_name} /bin/false"
+            blacklist_line = f"blacklist {module_name}"
+            
+            has_required = (install_line in file_lines and blacklist_line in file_lines)
+            
+            other_mentions = [
+                l for l in file_lines 
+                if module_name in l and l != install_line and l != blacklist_line
+            ]
+            
+            module_status[module_name] = has_required and not other_mentions
+        
+        return module_status
+    
+    def update_config_file_contents(self):
+        with open(self.config_file, 'r') as f:
+            lines = f.readlines()
 
-    if modules_list:
-        header_needs_change = not lines or lines[0].rstrip('\n') != comment
-        if header_needs_change:
-            lines = [comment + '\n'] + lines
-            content_changed = True
+        new_lines = [lines[0]]
 
-        for mod in modules_list:
-            for is_install in [True, False]:
-                if is_install:
-                    target_line = f"install {mod} /bin/false"
-                    regexp_str = r"^(#)?install\s+" + re.escape(mod) + r"\s"
-                else:
-                    target_line = f"blacklist {mod}"
-                    regexp_str = r"^(#)?blacklist\s+" + re.escape(mod) + r"$"
+        for line in lines[1:]:
+            if not any(m in line for m in self.modules):
+                new_lines.append(line)
 
-                regexp = re.compile(regexp_str)
+        if not new_lines[-1].endswith('\n'):
+            new_lines[-1] += '\n'
 
-                found_index = -1
-                for i, line in enumerate(lines):
-                    if regexp.match(line.rstrip('\n')):
-                        found_index = i
+        for module_name in self.modules:
+            new_lines.append(f"install {module_name} /bin/false\n")
+            new_lines.append(f"blacklist {module_name}\n")
 
-                this_needs_change = False
-                if found_index != -1:
-                    current_line = lines[found_index].rstrip('\n')
-                    if current_line != target_line:
-                        this_needs_change = True
-                        if not module.check_mode:
-                            lines[found_index] = target_line + '\n'
-                else:
-                    this_needs_change = True
-                    if not module.check_mode:
-                        lines.append(target_line + '\n')
+        with open(self.config_file, 'w') as f:
+            f.writelines(new_lines)
 
-                if this_needs_change:
-                    content_changed = True
+    def validate_current_state(self):
+        if not self.check_config_file_exists():
+            return {
+                "contents": {m: False for m in self.modules},
+                "exists":   False,
+                "group":    False,
+                "header":   False,
+                "mode":     False,
+                "unloaded": {m: False for m in self.modules},
+                "user":     False,
+            }
+        
+        return {
+            "contents": self.check_config_file_contents(),
+            "exists":   True,
+            "group":    self.check_config_file_group(),
+            "header":   self.check_config_file_header(),
+            "mode":     self.check_config_file_mode(),
+            "unloaded": self.check_modules_unloaded(),
+            "user":     self.check_config_file_user(),
+        }
 
-        if content_changed:
-            changed = True
+    def create_config_file(self):
+        with open(self.config_file, 'w') as f:
+            f.write(f"{self.FILE_HEADER}\n")
+            for module_name in self.modules:
+                f.write(f"install {module_name} /bin/false\n")
+                f.write(f"blacklist {module_name}\n")
+        
+        os.chown(self.config_file, self.FILE_UID, self.FILE_GID)
+        os.chmod(self.config_file, self.FILE_MODE)
+    
+    def check_modules_unloaded(self):
+        current_loaded = [line.split()[0] for line in open('/proc/modules').readlines()]
+        return {m: (m not in current_loaded) for m in self.modules}
 
-    if not module.check_mode:
-        if modules_list and (content_changed or not file_exists):
-            with open(config_file, 'w') as f:
-                f.writelines(lines)
+    def unload_modules(self, modules_to_unload):
+        for m in modules_to_unload:
+            self.module.run_command([self.module.get_bin_path('modprobe', True), '-r', m])
 
-        need_set_perms = owner_needs_change or mode_needs_change or (modules_list and (content_changed or not file_exists))
-        if need_set_perms:
-            os.chown(config_file, 0, 0)
-            os.chmod(config_file, 0o644)
-
-    unloaded_modules = []
-    if modules_list:
-        try:
-            lsmod_output = subprocess.check_output(['lsmod'], text=True)
-            loaded_modules = set()
-            for line in lsmod_output.splitlines()[1:]:
-                if line.strip():
-                    loaded_modules.add(line.split()[0])
-        except Exception as e:
-            module.fail_json(msg=f"Failed to check loaded modules: {str(e)}", **result)
-
-        for mod in modules_list:
-            if mod in loaded_modules:
-                if not module.check_mode:
-                    try:
-                        subprocess.check_call(['modprobe', '-r', mod])
-                    except subprocess.CalledProcessError:
-                        pass
-                unloaded_modules.append(mod)
-                changed = True
-
-    result['changed'] = changed
-    result['unloaded_modules'] = unloaded_modules
-
-    message_parts = [
-        f'Ensured configuration file {config_file} exists with correct ownership, '
-        f'permissions, managed header, and kernel module configurations.'
-    ]
-
-    if module._verbosity >= 1:
-        if result['skipped_modules']:
-            skip_msg = f'Skipped modules: {", ".join(result["skipped_modules"])} ({result["debug_message"]}).'
-            message_parts.append(skip_msg)
-    else:
-        if 'skipped_modules' in result:
-            del result['skipped_modules']
-        if 'debug_message' in result:
-            del result['debug_message']
-
-    result['message'] = ' '.join(message_parts)
-
-    module.exit_json(**result)
 
 def main():
-    run_module()
+    module = AnsibleModule(
+        argument_spec=dict(
+            config_file=dict(type='str', required=False, default='/etc/modprobe.d/cis_hardening.conf'),
+            modules=dict(type='list', elements='str', required=True),
+        ),
+        supports_check_mode=True,
+    )
 
-if __name__ == '__main__':
+    manager  = ConfigureFilesystemKernelModules(module)
+    skip_msg = manager.filter_modules()
+    results  = manager.validate_current_state()
+    
+    if not results['exists']:
+        if not module.check_mode:
+            manager.create_config_file()
+        
+        module.exit_json(
+            changed=True,
+            msg=f"Created {manager.config_file} with required settings.",
+            validation_results=manager.validate_current_state() if not module.check_mode else results
+        )
+
+    changed = False
+    change_msg = []
+
+    if not results['mode']:
+        changed = True
+        change_msg.append("updated file mode")
+        if not module.check_mode:
+            manager.update_config_file_mode()
+    
+    if not results['user'] or not results['group']:
+        changed = True
+        
+        if not results['user'] and not results['group']:
+            change_msg.append("updated file user and group")
+        elif not results['user']:
+            change_msg.append("updated file user")
+        else:
+            change_msg.append("updated file group")
+
+        if not module.check_mode:
+            manager.update_config_file_ownership()
+
+    if not results['header']:
+        changed = True
+        change_msg.append("updated header")
+        if not module.check_mode:
+            manager.update_config_file_header()
+
+    if not all(results['contents'].values()):
+        changed = True
+        content_modules = [m for m, ok in results['contents'].items() if not ok]
+        change_msg.append(f"synchronized modules: {', '.join(content_modules)}")
+        
+        if not module.check_mode:
+            manager.update_config_file_contents()
+
+    to_unload = [m for m, is_unloaded in results['unloaded'].items() if not is_unloaded]
+
+    if to_unload:
+        changed = True
+        change_msg.append(f"unloaded modules: {', '.join(to_unload)}")
+
+        if not module.check_mode:
+            manager.unload_modules(to_unload)
+
+    if skip_msg:
+        final_msg = f"{skip_msg}; {final_msg}"
+
+    module.exit_json(
+        changed=changed,
+        msg="; ".join(change_msg) if changed else "File matches specification",
+        validation_results=manager.validate_current_state() if (changed and not module.check_mode) else results
+    )
+
+if __name__ == "__main__":
     main()
